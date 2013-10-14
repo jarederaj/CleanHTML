c52fa9af815d4b066f5490700031a5df38cdce99
Jared <jrhall@gmail.com>
2013-10-14 14:41:27 -0600

    built out the basic script.  Pretty simple implementation of beautiful soup.

--- README.md
+++ README.md
@@ -2,22 +2,27 @@ CleanHTML
 =========
 
 Quick Script to pretty print HTML from command line that uses BeautifulSoup to 
-create a new cleanly formatted file.  Nothing fancy here... just something for 
-command line noobs to work with.
+create a new cleanly formatted file.  Nothing fancy here, just something for 
+shell noobs to work with... exactly the sort that might need this script 
+written out for them.
 
 Requirements
 ------------
 
 * A compupter
-* Python 2.7 or higher
+* Python 
 
 Examples
 -------
     
-    python /path/to/CleanHTML/cleanit.py /path/to/your/ugly.html
+    python /path/to/CleanHTML/cleanit.py -i /path/to/your/ugly.html
 
 This will create a new file:
     
     /path/to/your/ugly.html.clean
 
 which you can edit, diff, or use to overwrite the existing file.
+
+You might also:
+
+    python /path/to/CleanHTML/cleanit.py -i /path/to/your/old/ugly.html -o /path/to/your/new/pretty.html
---
+++ cleanit.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+import sys, getopt, bs4
+from lib.beautiful_soup import BeautifulSoup
+
+class Cleanit():
+
+    def main(self, argv):
+        arguments = self.parseArguments(argv)
+        try:
+            with open(arguments['input_file'], mode="r") as f:
+                soup = BeautifulSoup(f, indentWidth='    ')
+        except IOError:
+            self.cleanExit("Error: Could not open the input file!")
+
+        try:
+            output = open(arguments['output_file'].encode('utf-8','ignore'), "w")
+            output.write(soup.prettify())
+            output.close()
+        except IOError:
+            self.cleanExit("Error: Could not write to the output file!")
+
+        sys.exit(0)
+
+        
+    def cleanExit(self, msg=""):
+        print msg
+        print 'cleanit.py -i <inputfile> [-o <outputfile>]'
+        sys.exit(1)
+
+    def parseArguments(self, argv):
+        input_file = False
+        output_file = False
+
+        try:
+            opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+        except getopt.GetoptError:
+            self.cleanExit()
+
+        for opt, arg in opts:
+            if opt in ("-i", "--ifile"):
+                input_file = arg
+            if opt == '-h':
+                self.cleanExit()
+            if opt in ("-o", "--ofile"):
+                output_file = arg
+
+        if not input_file:
+            self.cleanExit()
+
+        if not output_file:
+            output_file = "%s.clean" % (input_file)
+
+        return {
+            "input_file": input_file,
+            "output_file": output_file,
+        }
+
+if __name__ == "__main__":
+    Cleanit().main(sys.argv[1:])
